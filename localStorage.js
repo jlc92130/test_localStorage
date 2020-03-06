6ba7e8c4c4dd1c570f29d9ca97365fdd37054106
unknown <jl.crobeddu@codeur.online>
2020-03-06 09:54:27 +0100

    theme memorized page can be refreshed and keep the user savings

--- localStorage.js
+++ localStorage.js
@@ -4,16 +4,20 @@ let span = document.querySelector('span');
 function modeSombre() {
     document.body.className = "dark";
     span.textContent = "Theme clair";
-    //localStorage.setItem('theme', 'sombre');
+    localStorage.setItem('theme', 'sombre');
 
 }
-
+if(localStorage.getItem('theme')){
+    if(localStorage.getItem('theme') == "sombre"){
+        modeSombre();
+    }
+}
 
 bouton.addEventListener('click',() => {
     if(document.body.classList.contains('dark') === true) {
         document.body.className = "";
         span.textContent = "Theme sombre";
-        //localStorage.setItem('theme', 'clair');
+        localStorage.setItem('theme', 'clair');
     }
     else {
         modeSombre();
